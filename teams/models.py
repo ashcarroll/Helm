bd3ecf172c0bd013a97d6bce2027d2c4dd6c1d13
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-03-10 18:19:22 +0000

    Implemented Team model in the teams app

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    manager = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='managed_teams')
+    members = models.ManyToManyField(User, related_name='teams')
+
+    def __str__(self):
+        return self.name
