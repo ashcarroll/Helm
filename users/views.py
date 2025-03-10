622bc0780c01aed3bcafbed92b686d46080ac3eb
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-03-10 21:30:21 +0000

    Added user registration form with role choices for groups

--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import Group
+from django.contrib.auth import login
+from .forms import UserRegisterForm
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+
+            # Create a user but don't save it to the DB just yet
+            user = form.save(commit=False)
+
+            # Ensure email is stored
+            user.email = form.cleaned_data['email']
+            user.save() 
+
+            # Assign user to their chosen group, with some error handling
+            role = form.cleaned_data.get('role')
+            try: 
+                group = Group.objects.get(name=role)
+                group.user_set.add(user)
+            except Group.DoesNotExist:
+                messages.warning(request, f"Group '{role}' does not exist")
+            
+            # Log the user in straight away
+            # login(request, user) TO-DO: uncomment this and update redirect when dashboard is created
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
