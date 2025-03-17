ddf19e251f85f54712e50fd3dcf97b40cd247ff8
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-03-17 21:58:49 +0000

    Added basic dashboard view and template Set up dashboard URLs and routing

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def dashboard_view(request):
+    context = {
+        'username': request.user.username
+    }
+    return render(request, 'dashboard/dashboard.html', context)
