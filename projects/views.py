3d18b069a072c1122681e32e7273bee35876874c
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-03-18 00:25:35 +0000

    Added ProjectForm for new project creation Added ProjectCreateView with a role check for managers

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import CreateView
+from .models import Project
+from .forms import ProjectForm
 
-# Create your views here.
+class ProjectCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Project
+    form_class = ProjectForm
+    template_name = 'projects/create_project.html'
+
+    def form_valid(self, form):
+        project = form.save(commit=False)
+        project.manager = self.request.user
+        project.save()
+        messages.success(self.request, "Project has been created successfully")
+
+        return redirect('project_list')
+    
+    def test_func(self):
+        # Only managers can access this view
+        return self.request.user.groups.filter(name='Manager').exists()
+    
+    def handle_no_permission(self):
+        # Error handling to protect view on the backend, incase URL is guessed and UI constraints are bypassed
+        messages.error(self.request, "Only managers can create projects")
+        return redirect('project_list')
