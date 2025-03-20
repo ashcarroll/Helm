341bbc7ccce46e4ee485b9191e23841aabf07f8c
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-03-20 00:30:45 +0000

    Added url routing for project update and delete

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import ProjectListView, ProjectCreateView, ProjectDetailView, TaskCreateView, TaskUpdateView
+from .views import ProjectListView, ProjectCreateView, ProjectDetailView, ProjectUpdateView, ProjectDeleteView, TaskCreateView, TaskUpdateView
 
 urlpatterns = [
     path('list/', ProjectListView.as_view(), name='project_list'),
     path('<int:pk>/', ProjectDetailView.as_view(), name ='project_detail'),
     path('create/', ProjectCreateView.as_view(), name='project_create'),
+    path('<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
+    path('<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
     path('<int:project_id>/tasks/create/', TaskCreateView.as_view(), name='task_create'),
     path('tasks/<int:pk>/update', TaskUpdateView.as_view(), name='task_update'),
 ]
