6a3efbbb10d125e90858b2fad50e4a41bdbc2df4
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-04-04 14:39:03 +0100

    Added Profile model with image field to user app Added signals to auto create and save profiles for new users Configured MEDIA _URL and MEDIA_ROOT Updated helm/urls.py for media

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Helper function to define upload path for profile pics
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    # Using an upload_to function to organize uploads in folders by user ID
+    image = models.ImageField(upload_to=user_directory_path, default='default.jpg')
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
