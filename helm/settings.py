6e341ba3f1dd17ee49b34259b272b6a01a4421b5
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-04-06 13:58:26 +0100

    Set up database

--- helm/settings.py
+++ helm/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from dotenv import load_dotenv
 import os
 from pathlib import Path
+import dj_database_url
 
 load_dotenv()
 
@@ -85,12 +86,7 @@ WSGI_APPLICATION = 'helm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {"default": dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")}
 
 
 # Password validation
