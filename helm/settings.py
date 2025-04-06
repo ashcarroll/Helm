085cd708ce59f9da52ccfa1623faddae7d335688
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-04-06 15:17:24 +0100

    Set up new secret key

--- helm/settings.py
+++ helm/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g!y$r+#3s)*^*v4h(y1b)cf_$@5kntinfj%tno320vrb^u-%f&'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'TRUE'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_HOSTNAME', '')]
 
