d6a59cc0e2af33bbf5063853ec228223fd5c1c46
Aisling Carroll <aislingcarroll0801@gmail.com>
2025-04-11 11:18:03 +0100

    Added missing '/' in url to see if it fixes task completion chart 404

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,5 @@ from .views import dashboard_view, project_status_data, task_completion_data
 urlpatterns = [
     path('', dashboard_view, name='dashboard'),
     path('api/project-status-data/', project_status_data, name='project_status_data'),
-    path('api/task-completion-data', task_completion_data, name='task_completion_data'), 
+    path('api/task-completion-data/', task_completion_data, name='task_completion_data'), 
 ]
